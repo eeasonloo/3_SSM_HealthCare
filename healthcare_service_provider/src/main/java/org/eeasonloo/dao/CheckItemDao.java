65c5e32047524bb4a1d8b0ceae87b3c26b642422
eeasonloo <3281038296@qq.com>
2021-08-13 14:12:56 +0800

    CheckItemDao and Impl SelectByConditions SQL First Commit

--- healthcare_service_provider/src/main/java/org/eeasonloo/dao/CheckItemDao.java
+++ healthcare_service_provider/src/main/java/org/eeasonloo/dao/CheckItemDao.java
@@ -1,8 +1,12 @@
 package org.eeasonloo.dao;
 
+import com.github.pagehelper.Page;
+import org.eeasonloo.entity.PageResult;
 import org.eeasonloo.pojo.CheckItem;
 
 public interface CheckItemDao {
 
     public void add(CheckItem checkItem);
+
+    public Page<CheckItem> selectByCondition(String queryString);
 }
