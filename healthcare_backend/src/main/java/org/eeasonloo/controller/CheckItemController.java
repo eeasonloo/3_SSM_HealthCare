96089c16692713828dbfb426e384d9247829ec3b
eeasonloo <3281038296@qq.com>
2021-08-13 14:12:02 +0800

    CheckItemController findPage feature First Commit

--- healthcare_backend/src/main/java/org/eeasonloo/controller/CheckItemController.java
+++ healthcare_backend/src/main/java/org/eeasonloo/controller/CheckItemController.java
@@ -2,6 +2,8 @@ package org.eeasonloo.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import org.eeasonloo.constant.MessageConstant;
+import org.eeasonloo.entity.PageResult;
+import org.eeasonloo.entity.QueryPageBean;
 import org.eeasonloo.entity.Result;
 import org.eeasonloo.pojo.CheckItem;
 import org.eeasonloo.service.CheckItemService;
@@ -25,6 +27,15 @@ public class CheckItemController {
         }
         return new Result(true, MessageConstant.ADD_CHECKGROUP_SUCCESS);
 
+    }
+
+    @RequestMapping("/findPage")
+    public PageResult findPage(@RequestBody QueryPageBean queryPageBean){
+
+            PageResult pageResult = checkItemService.findPage(queryPageBean.getCurrentPage(),
+                    queryPageBean.getPageSize(),queryPageBean.getQueryString());
+
+        return pageResult;
     }
 
 
