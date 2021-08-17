4bf4637678eade7c262b670f891e765c2629145d
eeasonloo <3281038296@qq.com>
2021-08-17 14:35:42 +0800

    CheckGroup Controller Add CheckGroup Commit

---
+++ healthcare_backend/src/main/java/org/eeasonloo/controller/CheckGroupController.java
@@ -0,0 +1,30 @@
+package org.eeasonloo.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import org.eeasonloo.constant.MessageConstant;
+import org.eeasonloo.entity.Result;
+import org.eeasonloo.pojo.CheckGroup;
+import org.eeasonloo.service.CheckGroupService;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/checkgroup")
+public class CheckGroupController {
+
+    @Reference
+    private CheckGroupService checkGroupService;
+
+    @RequestMapping("/add")
+    public Result add(Integer[] checkitemIds, @RequestBody CheckGroup checkGroup){
+        try {
+            checkGroupService.add(checkitemIds,checkGroup);
+        } catch (Exception e) {
+            return new Result(false, MessageConstant.ADD_CHECKGROUP_FAIL);
+        }
+        return new Result(true, MessageConstant.ADD_CHECKGROUP_SUCCESS);
+    }
+
+
+}
