9e39259c7829d8b56b1aa71ae7349254675a1566
eeasonloo <3281038296@qq.com>
2021-08-18 14:56:23 +0800

    CheckgroupService and Impl FindPage Commit

--- healthcare_interface/src/main/java/org/eeasonloo/service/CheckGroupService.java
+++ healthcare_interface/src/main/java/org/eeasonloo/service/CheckGroupService.java
@@ -1,9 +1,12 @@
 package org.eeasonloo.service;
 
+import org.eeasonloo.entity.PageResult;
 import org.eeasonloo.pojo.CheckGroup;
 import org.springframework.web.bind.annotation.RequestBody;
 
 public interface CheckGroupService {
 
     public void add(Integer[] checkItemIds, @RequestBody CheckGroup checkGroup);
+
+    public PageResult findPage(Integer currentPage, Integer pageSize, String queryString);
 }
--- healthcare_service_provider/src/main/java/org/eeasonloo/service/impl/CheckGroupServiceImpl.java
+++ healthcare_service_provider/src/main/java/org/eeasonloo/service/impl/CheckGroupServiceImpl.java
@@ -1,13 +1,17 @@
 package org.eeasonloo.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import org.eeasonloo.dao.CheckGroupDao;
+import org.eeasonloo.entity.PageResult;
 import org.eeasonloo.pojo.CheckGroup;
 import org.eeasonloo.service.CheckGroupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service(interfaceClass = CheckGroupService.class)
@@ -23,6 +27,7 @@ public class CheckGroupServiceImpl implements CheckGroupService {
 
     }
 
+
     private void setCheckGroupAndCheckItem(Integer checkGroupId, Integer[] checkItemIds) {
 
         if(checkItemIds != null && checkItemIds.length > 0){
@@ -34,6 +39,13 @@ public class CheckGroupServiceImpl implements CheckGroupService {
                 checkGroupDao.setCheckGroupAndCheckItem(checkGroup_checkItemMap);
             }
         }
+    }
 
+    @Override
+    public PageResult findPage(Integer currentPage, Integer pageSize, String queryString) {
+        PageHelper.startPage(currentPage,pageSize);
+        Page<CheckGroup> page = checkGroupDao.findByCondition(queryString);
+
+        return new PageResult(page.getTotal(),page.getResult());
     }
 }
