5eada9877a1287cdcf3aaabe287d706e6096be31
eeasonloo <3281038296@qq.com>
2021-08-24 14:03:33 +0800

    SetmealContoller add setmeal and associations Commit

--- healthcare_backend/src/main/java/org/eeasonloo/controller/SetmealController.java
+++ healthcare_backend/src/main/java/org/eeasonloo/controller/SetmealController.java
@@ -1,7 +1,12 @@
 package org.eeasonloo.controller;
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import org.eeasonloo.constant.MessageConstant;
 import org.eeasonloo.entity.Result;
+import org.eeasonloo.pojo.Setmeal;
+import org.eeasonloo.service.CheckGroupService;
+import org.eeasonloo.service.SetmealService;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,12 +21,15 @@ import java.util.UUID;
 @RequestMapping("/setmeal")
 public class SetmealController {
 
+    @Reference
+    private SetmealService setmealService;
+
     @RequestMapping("/upload")
     public Result upload(@RequestParam("imgFile") MultipartFile imgFile) {
         try {
             String fileSavePath = "C:\\my_java\\repository\\healthcare_parent\\healthcare_backend\\src\\main\\webapp\\upload";
             // Generate a unique UUID name for image + suffix(.jpg)
-            String originalFilename  = imgFile.getOriginalFilename();
+            String originalFilename = imgFile.getOriginalFilename();
             int lastIndexOf = originalFilename.lastIndexOf(".");
             String suffix = originalFilename.substring(lastIndexOf);
             String newFilename = UUID.randomUUID().toString() + suffix;
@@ -29,13 +37,20 @@ public class SetmealController {
             File uploadSaveFile = new File(fileSavePath, newFilename);
             imgFile.transferTo(uploadSaveFile);
 
-            return new Result(true, MessageConstant.PIC_UPLOAD_SUCCESS,newFilename);
+            return new Result(true, MessageConstant.PIC_UPLOAD_SUCCESS, newFilename);
 
         } catch (IOException e) {
             return new Result(false, MessageConstant.PIC_UPLOAD_FAIL);
-
         }
+    }
 
-
+    @RequestMapping("/add")
+    public Result add(Integer[] checkgroupIds, @RequestBody Setmeal setmeal) {
+        try {
+            setmealService.add(checkgroupIds, setmeal);
+        } catch (Exception e) {
+            return new Result(false, MessageConstant.ADD_SETMEAL_FAIL);
+        }
+        return new Result(true, MessageConstant.ADD_SETMEAL_SUCCESS);
     }
 }
