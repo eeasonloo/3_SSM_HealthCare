997c998a6abc37c9b365e0211221851917387116
eeasonloo <3281038296@qq.com>
2021-08-26 14:15:25 +0800

    SetmealDao and Mapper findPage Commit

--- healthcare_service_provider/src/main/java/org/eeasonloo/dao/SetmealDao.java
+++ healthcare_service_provider/src/main/java/org/eeasonloo/dao/SetmealDao.java
@@ -1,5 +1,6 @@
 package org.eeasonloo.dao;
 
+import com.github.pagehelper.Page;
 import org.eeasonloo.pojo.Setmeal;
 
 import java.util.Map;
@@ -9,4 +10,6 @@ public interface SetmealDao {
     public void add(Setmeal setmeal);
 
     public void addAssociationsSetmealAndCheckgroup(Map<String, Integer> map);
+
+    public Page<Setmeal> findByCondition(String queryString);
 }
