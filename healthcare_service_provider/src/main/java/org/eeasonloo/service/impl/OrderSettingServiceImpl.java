8559aa7655e1e0595d3392dc16a828037250b834
eeasonloo <3281038296@qq.com>
2021-09-02 14:30:13 +0800

    OrderSettingServiceImpl Add OrderSetting Commit

--- healthcare_service_provider/src/main/java/org/eeasonloo/service/impl/OrderSettingServiceImpl.java
+++ healthcare_service_provider/src/main/java/org/eeasonloo/service/impl/OrderSettingServiceImpl.java
@@ -1,12 +1,39 @@
 package org.eeasonloo.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import org.eeasonloo.dao.OrderSettingDao;
+import org.eeasonloo.pojo.OrderSetting;
 import org.eeasonloo.service.OrderSettingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service(interfaceClass = OrderSettingService.class)
 @Transactional
-public class OrderSettingServiceImpl {
+public class OrderSettingServiceImpl implements OrderSettingService {
+
+    @Autowired
+    private OrderSettingDao orderSettingDao;
+
+    public void add(List<OrderSetting> dateAndOrdersList){
+
+        if(dateAndOrdersList != null && dateAndOrdersList.size() > 0){
+            for (OrderSetting orderSetting : dateAndOrdersList) {
+                int count = orderSettingDao.findCountByDate(orderSetting.getOrderDate());
+
+                if(count > 0){
+                    // Means current date existed, need to edit number
+                    orderSettingDao.editNumberByOrderDate(orderSetting);
+                }else{
+                    // Means current date doesnt existed, need to add both date, number
+                    orderSettingDao.add(orderSetting);
+                }
+            }
+
+        }
+
+    }
 
 
 }
