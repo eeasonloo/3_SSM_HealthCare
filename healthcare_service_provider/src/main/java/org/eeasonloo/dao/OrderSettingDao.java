f784edfdd7db40c34688f92377abbc21096126b0
eeasonloo <3281038296@qq.com>
2021-09-03 14:30:40 +0800

    OrderSettingDao and Mapper Reservations Calender Display Commit

--- healthcare_service_provider/src/main/java/org/eeasonloo/dao/OrderSettingDao.java
+++ healthcare_service_provider/src/main/java/org/eeasonloo/dao/OrderSettingDao.java
@@ -3,10 +3,13 @@ package org.eeasonloo.dao;
 import org.eeasonloo.pojo.OrderSetting;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 public interface OrderSettingDao {
 
     public int findCountByDate(Date orderDate);
     public void editNumberByOrderDate(OrderSetting orderSetting);
     public void add(OrderSetting orderSetting);
+    public List<OrderSetting> getOrderSettingByMonth(Map map);
 }
