766e3026b4694c710d90a2ca31871406c6304037
eeasonloo <3281038296@qq.com>
2021-09-09 14:36:56 +0800

    Healthcare Mobile Setmeal Contoller FindAll Setmeal Commit

---
+++ healthcare_mobile/src/main/java/org/eeasonloo/controller/SetmealController.java
@@ -0,0 +1,31 @@
+package org.eeasonloo.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import org.eeasonloo.constant.MessageConstant;
+import org.eeasonloo.entity.Result;
+import org.eeasonloo.pojo.Setmeal;
+import org.eeasonloo.service.SetmealService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/setmeal")
+public class SetmealController {
+
+    @Reference
+    private SetmealService setmealService;
+
+    @RequestMapping("/getSetmeal")
+    public Result getSetmeal(){
+        try {
+            List<Setmeal> setmealList = setmealService.findAll();
+            return new Result(true, MessageConstant.QUERY_SETMEAL_SUCCESS, setmealList);
+        } catch (Exception e) {
+            return new Result(false, MessageConstant.QUERY_SETMEAL_FAIL);
+        }
+
+    }
+
+}
