4bd8297095b5b2ee4ace4bd1aaea2ef2412f67b0
eeasonloo <3281038296@qq.com>
2021-09-09 14:35:43 +0800

    Healthcare SetmealDao and Mapper FindAll Setmeal Commit

--- healthcare_service_provider/src/main/java/org/eeasonloo/dao/SetmealDao.java
+++ healthcare_service_provider/src/main/java/org/eeasonloo/dao/SetmealDao.java
@@ -3,6 +3,7 @@ package org.eeasonloo.dao;
 import com.github.pagehelper.Page;
 import org.eeasonloo.pojo.Setmeal;
 
+import java.util.List;
 import java.util.Map;
 
 public interface SetmealDao {
@@ -12,4 +13,7 @@ public interface SetmealDao {
     public void addAssociationsSetmealAndCheckgroup(Map<String, Integer> map);
 
     public Page<Setmeal> findByCondition(String queryString);
+
+    public List<Setmeal> findAll();
+
 }
