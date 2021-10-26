9b9fa4f93c533e67fa660244cd8d22ff77a75772
eeasonloo <3281038296@qq.com>
2021-10-26 11:52:43 +0800

    test Mail Utils Encapsulated Commit

---
+++ healthcare_mobile/src/test/java/testMail.java
@@ -0,0 +1,12 @@
+import org.eeasonloo.constant.RedisMessageConstant;
+import org.eeasonloo.utils.MailUtils;
+
+public class testMail {
+
+    public static void main(String[] args) {
+
+        MailUtils.sendShortMessage("002", "easonloo1998@gmail.com", "1234");
+
+    }
+
+}
