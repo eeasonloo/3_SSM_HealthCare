55758de0cdf68f3be75d012799fdf210acd1ec4e
eeasonloo <3281038296@qq.com>
2021-10-27 11:59:57 +0800

    Member Pojo Update Commit

--- healthcare_common/src/main/java/org/eeasonloo/pojo/Member.java
+++ healthcare_common/src/main/java/org/eeasonloo/pojo/Member.java
@@ -12,10 +12,10 @@ public class Member implements Serializable{
     private String name;//姓名
     private String sex;//性别
     private String idCard;//身份证号
-    private String phoneNumber;//手机号
+    private String email;//手机号
     private Date regTime;//注册时间
     private String password;//登录密码
-    private String email;//邮箱
+    private String phoneNumber;//邮箱
     private Date birthday;//出生日期
     private String remark;//备注
 
