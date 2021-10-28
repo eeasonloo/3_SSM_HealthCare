3e21755420364679dc7795126e322c45e12c1f22
eeasonloo <3281038296@qq.com>
2021-10-28 12:05:52 +0800

    Validate Code Update Commit

--- healthcare_mobile/src/main/java/org/eeasonloo/controller/ValidateCodeContoller.java
+++ healthcare_mobile/src/main/java/org/eeasonloo/controller/ValidateCodeContoller.java
@@ -17,6 +17,7 @@ public class ValidateCodeContoller {
     @Autowired
     private JedisPool jedisPool;
 
+    @RequestMapping("/send4Order")
     public Result send4Order(String emailAddress){
         Integer code = ValidateCodeUtils.generateValidateCode(4);
 
@@ -32,6 +33,28 @@ public class ValidateCodeContoller {
         //将生成的验证码缓存到redis
         jedisPool.getResource().setex(
                 emailAddress + RedisMessageConstant.SENDTYPE_ORDER,5 * 60,code.toString());
+        //验证码发送成功
+        return new Result(true,MessageConstant.SEND_VALIDATECODE_SUCCESS);
+    }
+
+
+    //手机快速登录时发送手机验证码
+    @RequestMapping("/send4Login")
+    public Result send4Login(String email){
+        Integer code = ValidateCodeUtils.generateValidateCode(6);//生成6位数字验证码
+        try {
+            //发送短信
+            MailUtils.sendShortMessage(MailUtils.VALIDATE_CODE,email,code.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+            //验证码发送失败
+            return new Result(false, MessageConstant.SEND_VALIDATECODE_FAIL);
+        }
+        System.out.println("发送的手机验证码为：" + code);
+        //将生成的验证码缓存到redis
+        jedisPool.getResource().setex(email+RedisMessageConstant.SENDTYPE_LOGIN,
+                5 * 60,
+                code.toString());
         //验证码发送成功
         return new Result(true,MessageConstant.SEND_VALIDATECODE_SUCCESS);
     }
