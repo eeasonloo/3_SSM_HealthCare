e8cdd164e26286a377e9acf1f48f06475270184d
eeasonloo <3281038296@qq.com>
2021-10-31 11:57:14 +0800

    User Controller display logged in username Commit

---
+++ healthcare_backend/src/main/java/org/eeasonloo/controller/UserController.java
@@ -0,0 +1,25 @@
+package org.eeasonloo.controller;
+
+import org.eeasonloo.constant.MessageConstant;
+import org.eeasonloo.entity.Result;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/user")
+@RestController
+public class UserController {
+
+    //获取当前登录用户的用户名
+    @RequestMapping("/getUsername")
+    public Result getUsername()throws Exception{
+        try{
+            org.springframework.security.core.userdetails.User user =
+                    (org.springframework.security.core.userdetails.User)
+                            SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            return new Result(true, MessageConstant.GET_USERNAME_SUCCESS,user.getUsername());
+        }catch (Exception e){
+            return new Result(false, MessageConstant.GET_USERNAME_FAIL);
+        }
+    }
+}
